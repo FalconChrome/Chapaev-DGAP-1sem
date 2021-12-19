26287c9174fce92c6b6fcf38d05cf6223e1d79fd
Vorobev-Vova <91051346+Vorobev-Vova@users.noreply.github.com>
2021-12-19 19:08:27 +0300

    другое название

--- checkers.py
+++ checkers.py
@@ -1,32 +1,44 @@
 import pygame
-from pygame import sprite
 import math
 
-class Checker(sprite.Sprite):
-
-    def __init__(self, x,y,Vx,Vy,checkergroup,*groups,radius=37.5):
-        super().__init__(checkergroup,*groups)
+class Checker:
+    def __init__(self, x,y,Vx,Vy,radius=37.5):
         self.x  =  x
         self.y  =  y
         self.Vx  =  Vx
         self.Vy  =  Vy
         self.radius = radius
-        self.image = pygame.Surface()
-        self.radiusect = pygame.Rect(x-self.radius,y-self.radius,
-                                     2*self.radius,2*self.radius)
+
+    def distance2(self, other):
+        return (self.x - other.x) ** 2 + (self.y - other.y) ** 2
+
+    def distance2_to_pos(self, pos):
+        return (self.x - pos[0]) ** 2 + (self.y - pos[1]) ** 2
+
     def move(self,):
+        # if self.Vy != 0.0:
+        #     print(self.x, self.Vx, self.y)
         self.x+=self.Vx    #Перемещение
         self.y+=self.Vy
-        self.Vx-=self.Vx*0.01 # вязкое трение
-        self.Vy-=self.Vy*0.01
-    def update(self,):
+        self.Vx-=self.Vx*0.04 # вязкое трение
+        self.Vy-=self.Vy*0.04
+        if 0 < self.Vx ** 2 < 0.01:
+            self.Vx = 0
+        if 0 < self.Vy ** 2 < 0.01:
+            self.Vy = 0
+
+    def update(self, others):
         self.move()
-        for checker in sprite.spritecollide(self,self.groups()[0],False,
-                                            sprite.collide_circle):
-            self.hit(checker) #проверка на столкновение
+        for other in others:
+            # if self.distance2(other) not in (0.0, 551250.0):
+            #     print(self.distance2(other))
+            if 0 < self.distance2(other) <= (self.radius + other.radius) ** 2:
+                self.hit(other)
+
     def hit(self,other):
-        self.x+=-2*self.Vx
-        self.y+=-2*self.Vy
+        print("hit checker")
+        self.x+=-self.Vx
+        self.y+=-self.Vy
         gamma=math.atan2(other.y-self.y,other.x-self.x)
         betta=math.atan2(self.Vy,self.Vx)
         alfa=abs(gamma-betta)
@@ -35,9 +47,6 @@ class Checker(sprite.Sprite):
         V1=V0*math.cos(alfa)
         other.Vx=V1*math.cos(gamma)
         other.Vy=V1*math.sin(gamma)
-        print(gamma)
-        print(alfa)
-        print(betta)
         if self.Vy<0:
             if abs(gamma)<abs(betta):
                 self.Vx=V*math.cos(-3.14159/2+gamma)#FIX
@@ -57,12 +66,12 @@ class Checker(sprite.Sprite):
         other.y+=other.Vy
         other.x+=other.Vx
 
-        
-        
+    def kick(self, boost):
+        self.Vx += boost[0]
+        self.Vy += boost[1]
 
-#функция генерирует два ряда шашек
-        
-    
+    def get_pos(self):
+        return self.x, 0, self.y
 
-
-
+    def resting(self):
+        return self.Vx == 0 and self.Vy == 0
